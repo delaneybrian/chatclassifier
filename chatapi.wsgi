c16e1214b857b8e872384ff46ba578427d8a2d4e
delaneybrian <delaneybrian6@gmail.com>
2018-07-14 12:01:41 +0100

    added exception handling and updated apache config

--- chatapi.wsgi
+++ chatapi.wsgi
@@ -1,3 +1,3 @@
 import sys
-sys.path.insert(0, "/var/www/chatclassifier")
+sys.path.insert(0, "/var/www/ChatClassifier")
 from chatclassifier import app as application
--- chatclassifier.py
+++ chatclassifier.py
@@ -5,27 +5,38 @@ app = Flask(__name__)
 
 @app.route('/trainchat')
 def train_chat():
-    provider = Provider()
-    chat_id = request.args.get('chatId')
-    provider.train_chat_model(chat_id)
+    try:
+        provider = Provider()
+        chat_id = request.args.get('chatId')
+        provider.train_chat_model(chat_id)
+    except:
+        print("Could Not Train Chat")
 
 @app.route('/makeprediction')
 def make_prediction():
-    provider = Provider()
-    chat_id = request.args.get('chatId')
-    message = request.args.get('message')
-    prediction = provider.make_prediction_from_chat_model(chat_id, message)
-
-    return jsonify(prediction[0])
+    try:
+        provider = Provider()
+        chat_id = request.args.get('chatId')
+        message = request.args.get('message')
+        prediction = provider.make_prediction_from_chat_model(chat_id, message)
+        return jsonify(prediction[0])
+    except:
+        print("Could Not Make Prediction")
 
 @app.route('/createmoviesdf')
 def create_movies_df():
-    provider = Provider()
-    provider.add_new_movies_df()
+    try:
+        provider = Provider()
+        provider.add_new_movies_df()
+    except:
+        print("Could Not Create Movies DF")
 
 @app.route('/chatmoviescomparison')
 def chat_movies_comparison():
-    provider = Provider()
-    chat_id = request.args.get('chatId')
-    comparisons = provider.find_movie_comparisons_from_chat(chat_id)
-    return jsonify(comparisons)
+    try:
+        provider = Provider()
+        chat_id = request.args.get('chatId')
+        comparisons = provider.find_movie_comparisons_from_chat(chat_id)
+        return jsonify(comparisons)
+    except:
+        print("Count Not Create Movies Comparison")
