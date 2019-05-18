6f1f1c3514bfad875831a0ab5b9aea7b242cddaf
= <=>
2019-05-18 19:41:54 +0100

    return exception

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def make_prediction():
         return jsonify(prediction[0])
     except Exception as e:
         app.logger.info('Could Not Make Prediction - {}'.format(e))
-        return "False"
+        return e
 
 @app.route('/createmoviesdf')
 def create_movies_df():
